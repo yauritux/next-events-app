a27eb84eacd11839051383e78b14ae9394d67fbb
Yauri Attamimi <yauritux@gmail.com>
2023-11-26 21:58:05 +0700

    feat: add new function to filter event based on selected criteria

--- providers/EventRepository.ts
+++ providers/EventRepository.ts
@@ -14,6 +14,13 @@ export function allEvents(): Event[] {
   return data;
 }
 
+export function filterEvent(year: number, month: number) {
+  return data.filter((event) => {
+    const eventDate = event.date.split("-"); // 2023-12-25 => [2023, 12, 25]
+    return +eventDate[0] === year && +eventDate[1] === month;
+  });
+}
+
 export function featuredEvents(): Event[] {
   return data.filter((event) => event.isFeatured);
 }
