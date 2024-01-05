3ebb7b578ddf3323a6c4454b8f51bdbb34571127
Yauri Attamimi <yauritux@gmail.com>
2024-01-05 22:53:24 +0700

    integrate featuredEvents and dataSeed apis

--- providers/EventDBRepository.ts
+++ providers/EventDBRepository.ts
@@ -8,3 +8,17 @@ export async function allEvents() {
   console.log(JSON.stringify(response.data, null, 2));
   return response.data;
 }
+
+export async function featuredEvents() {
+  const response = await axios.get("http://localhost:3000/api/featured_events");
+
+  console.log(JSON.stringify(response.data, null, 2));
+  return response.data;
+}
+
+export async function initializeDataSeed() {
+  const response = await axios.get("http://localhost:3000/api/dataseed");
+
+  console.log(JSON.stringify(response.data, null, 2));
+  return response.data;
+}
