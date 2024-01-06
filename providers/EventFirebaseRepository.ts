440ada7f0f90e40766a711f1e71f10d205a64cb1
Yauri Attamimi <yauritux@gmail.com>
2024-01-06 23:29:45 +0700

    feat: new repository methods for finding an event by id and filter events by year and month

--- providers/EventFirebaseRepository.ts
+++ providers/EventFirebaseRepository.ts
@@ -1,10 +1,12 @@
 import { Event } from "@/models/Event";
+import axios from "axios";
 
 export async function allEvents() {
-  const response = await fetch(
+  const response = await axios.get(
     "https://nextjs-course-20910-default-rtdb.firebaseio.com/events.json"
   );
-  const data: any = response.json();
+
+  const data: any = response.data;
 
   const events: Event[] = [];
 
@@ -21,4 +23,27 @@ export async function allEvents() {
 export async function featuredEvents() {
   const eventRecords = await allEvents();
   return eventRecords.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id: string | string[] | undefined) {
+  const eventRecords = await allEvents();
+  return eventRecords.find((event) => event.id === id);
+}
+
+export async function filterEvents(dateFilter: {
+  year: number;
+  month: number;
+}) {
+  const { year, month } = dateFilter;
+
+  const eventRecords = await allEvents();
+
+  let filteredEvents = eventRecords.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
 }
